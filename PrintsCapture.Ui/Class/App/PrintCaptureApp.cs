c74d8e6ae4a2dbaf2701e216bb92df1cc34e85ab
Math Morn <rethdakair@hotmail.com>
2023-01-14 14:23:10 -0500

    change version number

--- PrintsCapture.Ui/Class/App/PrintCaptureApp.cs
+++ PrintsCapture.Ui/Class/App/PrintCaptureApp.cs
@@ -34,7 +34,7 @@ namespace PrintsCapture.Ui.Class
 
     public class PrintCaptureApp
     {
-        public const string AppVersion = "1.0.55.2";
+        public const string AppVersion = "1.0.55.3";
 
         public const string AppName = "PrintsCapture";
 
