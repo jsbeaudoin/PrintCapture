582f27fd59f5a002e044bd20e85a620b48163df2
rethdakair <rethdakair@gmail.com>
2024-09-15 11:01:09 -0400

    Handling dry skin enhancement as property

--- LivescanThales/Plugin/DeviceApi.cs
+++ LivescanThales/Plugin/DeviceApi.cs
@@ -40,6 +40,7 @@ namespace PrintsCapture.Device.LivescanThales.Plugin
         private const string HardwareMakeName = "THALES";
 
         const string PropFlatAutoScanEnabled = "FLAT_AUTO_SCAN";
+        const string PropDryEnhancementEnabled = "DRY_ENHANCE";
         const string PropMinPixelCount = "MIN_PIX_COUNT";
         const string PropErrorBeforeStopCount = "ERR_STOP_COUNT";
 
@@ -143,6 +144,7 @@ namespace PrintsCapture.Device.LivescanThales.Plugin
         {
             this.Properties = new CustomPropertyList();
             this.Properties.AddBoolProperty(PropFlatAutoScanEnabled, CommonText.PropertyEnabled, true, CommonText.PropertyFlatAutoCapture);
+            this.Properties.AddBoolProperty(PropDryEnhancementEnabled, CommonText.PropertyEnabled, false, CommonText.PropertyDryEnhance);            
             this.Properties.AddIntProperty(PropMinPixelCount, "Min pixel count", 500, "Image");
             this.Properties.AddIntProperty(PropErrorBeforeStopCount, "Error count to stop", 3, "Image");
             
@@ -205,7 +207,14 @@ namespace PrintsCapture.Device.LivescanThales.Plugin
                 this.ChangeState(DeviceState.Closed);
                 return;
             }
-           
+
+            bool enhance = this.Properties.GetBoolValue(PropDryEnhancementEnabled);
+            if (this.CheckIfError(GBMSAPI_NET_ScanSettingsRoutines.GBMSAPI_NET_EnableDrySkinImgEnhance(enhance))) {
+                this.OnDeviceOpened(DeviceOpenStatus.ErrorOccured);
+                this.ChangeState(DeviceState.Closed);
+                return;
+            }
+            
             this.currentAction = ScannerAction.Waiting;
             this.IsOpened = true;
             this.ChangeState(DeviceState.Opened);
